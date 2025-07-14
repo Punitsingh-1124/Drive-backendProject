9617958c21f8049af1c69d5119a608aeb9e52f95
Punitsingh-1124 <punitsingh2413@gmail.com>
2025-07-14 16:51:38 +0530

    backend Pj

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const userRouter = require("./routes/user.routes")
+const dotenv = require('dotenv');
+dotenv.config();
+const connectToDB = require('./config/db')
+connectToDB();
+
+dotenv.config();
 const app = express();
 
 app.set('view engine', 'ejs');
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 
 // @ route /user/test 
@@ -8,25 +10,35 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 router.post('/register',
-    body('email').trim().isEmail().isLength({min: 13}),
+    body('email').trim().isEmail().isLength({ min: 13 }),
     body('password').trim().isLength({ min: 5 }),
     body('username').trim().isLength({ min: 3 }),
-    (req, res) => {
+    async (req, res) => {
 
         const errors = validationResult(req);
-        if(!errors.isEmpty()){
+        if (!errors.isEmpty()) {
             return res.status(400).json({
                 errors: errors.array(),
                 message: 'Invalid data'
             })
         }
-        
-        console.log(req.body);
-        res.send(errors);
+
+        const { email, username, password } = req.body;
+
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        const newUser = await userModel.create({
+            email,
+            username,
+            password: hashPassword
+
+        });
+
+        res.json(newUser);
     });
 
 
 
 module.exports = router;
 
-//2:25:21 min
+//2:41:31 min
